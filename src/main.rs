97e318fb40aeb606b7afb2ec93a3a8ee8aee5dcb
KunoiSayami <i@leanhe.dev>
2023-05-30 01:37:03 +0800

    init
    Signed-off-by: KunoiSayami <i@leanhe.dev>

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::time::Duration;
+use rodio::{OutputStream, Sink, Source};
+use rodio::source::SineWave;
+
+
+fn main() -> anyhow::Result<()> {
+    let (_stream, stream_handle) = OutputStream::try_default()?;
+    let sink = Sink::try_new(&stream_handle)?;
+    let source = SineWave::new(440.0).repeat_infinite().amplify(0.0);
+    sink.append(source);
+
+
+    let b  = Arc::new(AtomicBool::new(false));
+
+    let new_b = b.clone();
+
+
+    ctrlc::set_handler(move || {
+        new_b.store(true, Ordering::Relaxed);
+    })?;
+
+    loop {
+        if b.load(Ordering::Relaxed) {
+            break
+        }
+        std::thread::sleep(Duration::from_millis(100));
+    }
+
+
+    Ok(())
+}
